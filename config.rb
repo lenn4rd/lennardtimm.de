73331f5c372c6b36fa7e3d72a0cdd31237aca7d1
Lennard Timm <hi@lenn4rd.io>
2019-10-07 22:08:20 +0200

    Create asset hashes on build

--- config.rb
+++ config.rb
@@ -22,6 +22,7 @@ page '/*.txt', layout: false
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
 configure :build do
+  activate :asset_hash
   activate :minify_css
   activate :minify_javascript
 end
